b22dcfb6fa76c2b86bd3430ec1954354cea8751c
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-02-28 20:05:07 +0330

    Adding locale utils  close #7

--- app/src/main/java/com/pureamorous/digikala/MainActivity.kt
+++ app/src/main/java/com/pureamorous/digikala/MainActivity.kt
@@ -5,11 +5,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.pureamorous.digikala.navigation.BottomNavigationBar
 import com.pureamorous.digikala.navigation.SetupNavGraph
 import com.pureamorous.digikala.ui.theme.DigikalaTheme
+import com.pureamorous.digikala.util.Constants.PERSIAN_LANG
+import com.pureamorous.digikala.util.LocaleUtils
 
 class MainActivity : ComponentActivity() {
     private lateinit var navController: NavHostController
@@ -20,16 +25,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             DigikalaTheme {
                 navController = rememberNavController()
-                Scaffold(
-                    bottomBar = {
-                        BottomNavigationBar(navController = navController, onItemClick = {
-                            navController.navigate(it.route)
-                        })
+                LocaleUtils.setLocate(LocalContext.current,PERSIAN_LANG)
+                CompositionLocalProvider(LocalLayoutDirection provides androidx.compose.ui.unit.LayoutDirection.Rtl ) {
+                    Scaffold(
+                        bottomBar = {
+                            BottomNavigationBar(navController = navController, onItemClick = {
+                                navController.navigate(it.route)
+                            })
+                        }
+                    ) {
+                        SetupNavGraph(navController = navController)
                     }
-                ) {
-                    SetupNavGraph(navController = navController)
                 }
-
             }
         }
     }
---
+++ app/src/main/java/com/pureamorous/digikala/util/Constnts.kt
@@ -0,0 +1,6 @@
+package com.pureamorous.digikala.util
+
+object Constants  {
+    const val ENGLISH_LANG = "en"
+    const val PERSIAN_LANG = "fa"
+}
