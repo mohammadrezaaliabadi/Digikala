dc44303a37e850cd343066ae777381bf69ea50e9
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-02-28 18:46:51 +0330

    Added splashScreen design close #4

--- app/src/main/java/com/pureamorous/digikala/MainActivity.kt
+++ app/src/main/java/com/pureamorous/digikala/MainActivity.kt
@@ -6,12 +6,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Scaffold
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.pureamorous.digikala.navigation.SetupNavGraph
 import com.pureamorous.digikala.ui.theme.DigikalaTheme
 
 class MainActivity : ComponentActivity() {
-    private lateinit var navController:NavController
+    private lateinit var navController: NavHostController
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/pureamorous/digikala/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/pureamorous/digikala/ui/screens/SplashScreen.kt
@@ -1,8 +1,58 @@
 package com.pureamorous.digikala.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.pureamorous.digikala.R
+import com.pureamorous.digikala.navigation.Screen
+import com.pureamorous.digikala.ui.components.Loading3Dots
+import com.pureamorous.digikala.ui.theme.splashBg
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
+    Splash()
+    LaunchedEffect(true) {
+        delay(2500)
+        navController.navigate(Screen.Home.route)
+    }
+}
+
+@Composable
+fun Splash() {
+    Box(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.splashBg)
+            .fillMaxSize(), contentAlignment = Alignment.Center
+    ) {
+        Image(
+            modifier = Modifier.size(250.dp),
+            painter = painterResource(id = R.drawable.digi_logo),
+            contentDescription = null
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(100.dp), contentAlignment = Alignment.BottomCenter
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.digi_txt_white),
+                contentDescription = null,
+                modifier = Modifier.height(30.dp)
+            )
+        }
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp), contentAlignment = Alignment.BottomCenter){
+            Loading3Dots(false)
+        }
+    }
 }
