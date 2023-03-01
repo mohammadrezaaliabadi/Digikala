d088be03fda4ac2ad26475ac763229ce4b17069d
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-03-01 11:21:40 +0330

    Added Multi lang and multi them close #10

--- app/src/main/java/com/pureamorous/digikala/ui/screens/BasketScreen.kt
+++ app/src/main/java/com/pureamorous/digikala/ui/screens/BasketScreen.kt
@@ -1,25 +1,69 @@
 package com.pureamorous.digikala.ui.screens
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import com.pureamorous.digikala.ui.theme.selectedBottomBar
 
 @Composable
 fun BasketScreen(navController: NavHostController) {
+    if (isSystemInDarkTheme()) {
+        BasketDark()
+    } else {
+        BasketLight()
+    }
+
+}
+
+@Composable
+fun BasketLight() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Blue),
+            .background(Color.LightGray),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "BasketScreen")
+        Text(
+            text = "BasketScreen",
+            color = MaterialTheme.colors.selectedBottomBar
+        )
     }
+}
 
+@Composable
+fun BasketDark() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.DarkGray),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "BasketScreen",
+            color = MaterialTheme.colors.selectedBottomBar
+        )
+    }
+}
+
+@Composable
+@Preview
+fun BasketLightPreview() {
+    BasketLight()
+}
+
+@Composable
+@Preview
+fun BasketDarkPreview() {
+    BasketDark()
 }
--- app/src/main/java/com/pureamorous/digikala/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/pureamorous/digikala/ui/screens/ProfileScreen.kt
@@ -1,18 +1,31 @@
 package com.pureamorous.digikala.ui.screens
 
+import android.app.Activity
+import android.content.Intent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.datastore.dataStore
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.pureamorous.digikala.MainActivity
+import com.pureamorous.digikala.util.Constants.ENGLISH_LANG
+import com.pureamorous.digikala.util.Constants.PERSIAN_LANG
+import com.pureamorous.digikala.viewmodel.DataStoreViewModel
 
 @Composable
-fun ProfileScreen(navController: NavHostController) {
+fun ProfileScreen(
+    navController: NavHostController,
+    dataStore: DataStoreViewModel = hiltViewModel()
+) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -20,7 +33,27 @@ fun ProfileScreen(navController: NavHostController) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        val activity = LocalContext.current as Activity
         Text(text = "ProfileScreen")
+
+        Button(onClick = {
+            dataStore.saveUserLanguage(PERSIAN_LANG)
+            activity.apply {
+                finish()
+                startActivity(Intent(activity, MainActivity::class.java))
+            }
+        }) {
+            Text(text = "fa")
+        }
+        Button(onClick = {
+            dataStore.saveUserLanguage(ENGLISH_LANG)
+            activity.apply {
+                finish()
+                startActivity(Intent(activity, MainActivity::class.java))
+            }
+        }) {
+            Text(text = "en")
+        }
     }
 
 }
